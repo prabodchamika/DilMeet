cf6d2e45b213375b69de2a5cc503767dea36c224
prabodchamika <157189856+prabodchamika@users.noreply.github.com>
2024-08-23 20:03:55 +0530

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,36 @@
+export const sidebarLinks = [
+  {
+    imgURL: '/icons/Home.svg',
+    route: '/',
+    label: 'Home',
+  },
+
+  {
+    imgURL: '/icons/upcoming.svg',
+    route: '/upcoming',
+    label: 'NextWave',
+  },
+  {
+    imgURL: '/icons/previous.svg',
+    route: '/previous',
+    label: 'LastCall',
+  },
+  {
+    imgURL: '/icons/Video.svg',
+    route: '/recordings',
+    label: 'Capture',
+  },
+  {
+    imgURL: '/icons/add-personal.svg',
+    route: '/personal-room',
+    label: 'Private Space',
+  },
+];
+
+export const avatarImages = [
+  '/images/avatar-1.jpeg',
+  '/images/avatar-2.jpeg',
+  '/images/avatar-3.png',
+  '/images/avatar-4.png',
+  '/images/avatar-5.png',
+];
